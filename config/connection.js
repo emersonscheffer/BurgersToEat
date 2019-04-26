a259e46babfc88670237b75fab88ef0207ad5622
Ermeson Scheffer <ermesonscheffer@Ermesons-MacBook-Pro.local>
2019-04-25 18:53:43 -0700

    Added connection to Jaws

---
+++ config/connection.js
@@ -0,0 +1,28 @@
+
+const mysql = require('mysql');
+
+if(process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
+
+
+
+connection.connect( err => {
+    if(err) {
+        console.log( 'error connecting: ' + err.stack);
+        return;
+    }
+
+    console.log('connected as id ' + connection.threadId);
+
+});
+
+module.exports = connection;
